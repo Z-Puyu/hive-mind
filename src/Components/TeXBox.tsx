829b4ba7fca1cb6906537cb65d1c91b0d0f3147f
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2023-05-24 20:50:11 +0800

    Temporarily delete some files and experiment a reworked version of TeX input

--- src/Components/TeXBox.tsx
+++ src/Components/TeXBox.tsx
@@ -1,47 +1,60 @@
-import { useState } from "react";
-import InlineMaths from './InlineMaths';
-import { MathJax } from 'better-react-mathjax';
+import { useState, useEffect } from 'react';
+import TeXBlock from './TeXBlock';
 
-interface MathObject {
-  isMath: boolean;
-  str: string;
+const { v4: uuidv4 } = require('uuid');
+
+export interface TeXBlockItem {
+  id: string;
+  html: string;
 }
 
-function TeXBox(): JSX.Element {
-  // Some default element to test functionality
-  const DEFAULT_DATA: MathObject[] = [
-    {
-      isMath: false,
-      str: "Click on ",
-    },
-    {
-      isMath: true,
-      str: "\\LaTeX",
-    },
-    {
-      isMath: false,
-      str: " to call out the LaTeX input panel, and click again to close it.",
-    },
-  ];
+export interface NewBlockData {
+  id: string;
+  ref: HTMLElement | null;
+}
 
-  const [mathObjects, setMathObjects] = useState<MathObject[]>(DEFAULT_DATA);
+export default function TeXBox(): JSX.Element {
+  const initBlock: TeXBlockItem = {
+    id: uuidv4(),
+    html: "This is the initial block, press Enter to type in a new block."
+  };
+
+  const [[TeXBlocks, currBlockData], setTeXBlocks] = useState<[TeXBlockItem[],
+    NewBlockData | undefined]>([[initBlock], undefined]);
+
+  useEffect(() => (currBlockData?.ref?.nextElementSibling as HTMLElement)?.focus(),
+    [currBlockData]);
+
+  const updateBoxHandler = (updatedBlock: TeXBlockItem) => {
+    const currBlockList: TeXBlockItem[] = TeXBlocks;
+    const updatedBlockIndex: number = currBlockList.map(blk => blk.id).indexOf(updatedBlock.id);
+    const updatedBlocks: TeXBlockItem[] = [...TeXBlocks];
+    updatedBlocks[updatedBlockIndex] = {
+      ...updatedBlocks[updatedBlockIndex],
+      html: updatedBlock.html,
+    };
+    setTeXBlocks([updatedBlocks, currBlockData]);
+  };
+
+  const addBlockHandler = (currBlock: NewBlockData) => {
+    const currBlockList: TeXBlockItem[] = TeXBlocks;
+    const currBlockIndex: number = currBlockList.findIndex(block => block.id === currBlock.id);
+    console.log(currBlockIndex);
+    const updatedBlocks: TeXBlockItem[] = [...currBlockList];
+    updatedBlocks.splice(currBlockIndex + 1, 0, { id: uuidv4(), html: "" });
+    setTeXBlocks([updatedBlocks, currBlock]);
+    console.log(TeXBlocks.length)
+    // (currBlock.ref?.nextElementSibling as HTMLElement)?.focus();
+  };
 
   return (
     <div>
-      <MathJax
-        contentEditable="true"
-        onKeyDown={event => {
-          // Press "$" to insert inline maths.
-          if (event.shiftKey && event.key === "$") {
-            event.preventDefault();
-            setMathObjects(mathObjects.concat([{ isMath: true, str: "" }]));
-          }
-        }}
-      >
-        {mathObjects.map(obj => obj.isMath ? <InlineMaths tex={obj.str} initTexVisibility={true}/> : <span>{obj.str}</span>)}
-      </MathJax>
+      {TeXBlocks.map(block => <TeXBlock
+        id={block.id}
+        html={block.html}
+        onAddBlock={addBlockHandler}
+        onUpdateBox={updateBoxHandler}
+      />)}
     </div>
   );
 }
-
-export default TeXBox;
