7e21d351549257bd9e25b7628637085531648d01
LzY <luo_zhiyang@outlook.com>
2023-05-28 22:22:52 +0800

    redux final change

--- src/Components/block.tsx
+++ src/Components/block.tsx
@@ -12,44 +12,39 @@ import { useSelector, useDispatch } from "react-redux";
 
 export default function Block(props: any): JSX.Element {
   const blocks = useSelector((state: any) => state.note.value);
-  console.log(blocks[0].html);
   
   const [innerHtml, setInnerHtml] = useState<string>(props.html);
   //const ref = useRef<HTMLElement>(null);
+
 
   const dispatch = useDispatch();
 
   /* No idea what this does but it is the equivalent of 
   the ComponentDidMount function from Konstantin's code */
-  // useEffect(() => {
-  //   dispatch(props.onUpdateBlock({
-  //     uid: props.uid,
-  //     html: innerHtml,
-  //   }))
-  // }, [innerHtml])
+  useEffect(() => {
+    dispatch(props.onUpdateBlock({
+      uid: props.uid,
+      html: innerHtml,
+    }))
+  }, [innerHtml])
 
   const HandleChange = (event:any) => {
     const updatedHtml = event.target.value;
     setInnerHtml(updatedHtml);
-    dispatch(props.onUpdateBlock({
-      uid: props.uid,
-      html: event.target.value,
-    }))
     // console.log(props.uid);
     // console.log(props.html);
     // console.log(props);
-    console.log(blocks[0].html);
+    //console.log(innerHtml);
   }
   
   const HandleKeyDown = (event:any) => {
     if (event.key === "Enter") {
       event.preventDefault();
-      // dispatch(props.onAddBlock({
-      //   uid: props.uid,
-      //   html: props.html,
-      // }));
-      //console.log(props);
-      console.log(blocks[0].html);
+      dispatch(props.onAddBlock({
+        uid: props.uid,
+        html: props.html,
+      }));  
+      console.log(innerHtml);
     }
     if (event.key === "Backspace" && props.html ==="") {
       event.preventDefault();
