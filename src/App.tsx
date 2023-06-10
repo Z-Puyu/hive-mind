81da6ad97bd3eca9c60899ea21d42c0031649c25
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2023-06-10 22:11:03 +0800

    Restore navigation bar and delete unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/NavBar";
 import Editor from "./pages/Editor";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export default function App(): JSX.Element {
   return (
     <div>
-      <Editor />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Editor" element={<Editor />} />
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 };
