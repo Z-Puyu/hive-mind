9e41ac88061b1e6c6bce6e352b0ac643f97738bd
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2023-07-20 01:18:29 +0800

    Delete src directory

--- src/utils/Comparator.tsx
+++
@@ -1,9 +0,0 @@
-export default function compare<T>(a: T, b: T, orderBy: keyof T, order: "asc" | "desc") {
-  if (b[orderBy] < a[orderBy]) {
-    return order === "desc" ? -1 : 1;
-  } 
-  if (a[orderBy] < b[orderBy]) {
-    return order === "desc" ? 1 : -1;
-  }
-  return 0;
-}
